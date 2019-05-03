75392a8c7fd19ef5e651a6b52a7e41d6d995f6ee
Patrick Biel <p.biel@outlook.com>
2019-05-03 18:47:18 -0400

    Create Tile Map

---
+++ Maze-Snake/Maze-Snake/Game Screen/ViewController/GameViewController.swift
@@ -0,0 +1,56 @@
+//
+//  GameViewController.swift
+//  Maze-Snake
+//
+//  Created by Period Four on 2019-05-02.
+//  Copyright © 2019 YBMW. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        // including entities and graphs.
+        if let scene = GKScene(fileNamed: "GameScene") {
+            
+            // Get the SKScene from the loaded GKScene
+            if let sceneNode = scene.rootNode as! GameScene? {
+                
+                // Copy gameplay related content over to the scene
+                sceneNode.entities = scene.entities
+                sceneNode.graphs = scene.graphs
+                
+                
+                
+                // Set the scale mode to scale to fit the window
+                sceneNode.scaleMode = .aspectFit
+                
+                
+                
+                // Present the scene
+                if let view = self.view as! SKView? {
+                    view.presentScene(sceneNode)
+                    
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
+            }
+        }
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.landscape
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
---
+++ Maze-Snake/Maze-Snake/Main Screen/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  Maze-Snake
+//
+//  Created by Patrick Biel on 2019-05-03.
+//  Copyright © 2019 YBMW. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.landscape
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
