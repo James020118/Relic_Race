fd70a95a33a08e3e8001c803c367355b270822ca
Patrick Biel <p.biel@outlook.com>
2019-05-16 19:28:09 -0400

    Partial Monster Implementation

--- Maze-Snake/Maze-Snake/Game Mechanics/Monster/Monster.swift
+++ Maze-Snake/Maze-Snake/Game Mechanics/Monster/Monster.swift
@@ -12,6 +12,7 @@ import GameplayKit
 
 class Monster: SKSpriteNode {
     
+    static let TILE_TRAVEL_TIME = 0.05
     
     init(texture: SKTexture, parent: GameScene) {
         let size = CGSize(width: 0, height: 0)
@@ -41,25 +42,33 @@ class Monster: SKSpriteNode {
         return rand.node
     }
     
-    func generatePath() {
+    func generatePath() -> SKAction {
         guard let scene = parent as? GameScene else {
-            return
+            return SKAction()
         }
         
-        //TODO:- Navigate through nodes in random directions to generate path
-        var nodes = scene.tileManager!.graph.nodes ?? []
+        //Navigate through nodes in random directions to generate path
         var path = [setRandomPosition()]
-        for i in 0...5 {
+        for _ in 0...5 {
             let node = path.last!
             let connectedNodes = node.connectedNodes
             let newNode = connectedNodes.randomElement()
-            //path.append(newNode!)
+            path.append(newNode! as! GKGridGraphNode)
         }
         
-        //TODO:- Once path of nodes is given,
-            //convert to grid position, then [action]
+        //Once path of nodes is given, convert to grid position, then [action]
+        var actions = [SKAction]()
+        for node in path {
+            let gridPos = GridPosition(from: node.gridPosition)
+            let newPos = scene.tileManager.getTile(row: gridPos.row, column: gridPos.column).position
+            let action = SKAction.move(to: newPos, duration: Monster.TILE_TRAVEL_TIME)
+            actions.append(action)
+        }
         
-        //TODO:- Create action sequence with [given [action], [action].reversed()] repeating
+        //Create action sequence with [given [action], [action].reversed()] repeating
+        actions += actions.reversed()
+        let pathAction = SKAction.sequence(actions)
+        return SKAction.repeatForever(pathAction)
     }
     
 }
