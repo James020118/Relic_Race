45cc2d512330576e3f8ccaa9f02365965daee288
Patrick Biel <p.biel@outlook.com>
2019-05-24 19:02:12 -0400

    Auto Layout

--- Maze-Snake/Maze-Snake/Main Screen/SettingsViewController.swift
+++ Maze-Snake/Maze-Snake/Main Screen/SettingsViewController.swift
@@ -17,10 +17,11 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
     
+    @IBAction func onBackButtonClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
     
 
 }
