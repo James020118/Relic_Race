10b88ec599e28f1ad3985d1cd6a30a76bf368a56
Patrick Biel <p.biel@outlook.com>
2019-05-24 23:35:02 -0400

    AutoLayout Changes

---
+++ Maze-Snake/Maze-Snake/Extensions/FitToBoundsLabel.swift
@@ -0,0 +1,33 @@
+//
+//  FitToBoundsLabel.swift
+//  Maze-Snake
+//
+//  Created by Patrick Biel on 2019-05-24.
+//  Copyright © 2019 YBMW. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UILabel {
+    
+    /// Will auto resize the contained text to a font size which fits the frames bounds.
+    /// Uses the pre-set font to dynamically determine the proper sizing
+    func fitTextToBounds() {
+        guard let text = text, let currentFont = font else { return }
+        
+        let bestFittingFont = UIFont.bestFittingFont(for: text, in: bounds, fontDescriptor: currentFont.fontDescriptor, additionalAttributes: basicStringAttributes)
+        font = bestFittingFont
+    }
+    
+    var basicStringAttributes: [NSAttributedString.Key: Any] {
+        var attribs = [NSAttributedString.Key: Any]()
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = self.textAlignment
+        paragraphStyle.lineBreakMode = self.lineBreakMode
+        attribs[.paragraphStyle] = paragraphStyle
+        
+        return attribs
+    }
+}
